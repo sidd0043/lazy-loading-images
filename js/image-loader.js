765af4fc03d2743ea360ec74087dd31224501f4a
sidd0043 <bigPapa@Mukkadass-MacBook-Pro.local>
2016-12-02 18:57:21 -0500

    java Script solved

--- js/image-loader.js
+++ js/image-loader.js
@@ -2,8 +2,8 @@ window.addEventListener('load', function (e) {
   var imgs = document. querySelectorAll('.non-critical-img');
 
   []. forEach.call(imgs, function (img){
-    var imgTag = img.querySelectorAll('img');
-    
+    var imgTag = img.querySelector('img');
+
     imgTag.src = imgTag.dataset.src;
     imgTag.removeAttribute('hidden');
     img.removeAttribute ('hidden');
